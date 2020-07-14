f3ab826826e53d5229bf2a831f46e937a3069847
davidguidali <davidguidali@gmail.com>
2020-07-14 18:18:46 +0200

    Added project and gitignore

---
+++ Volo.Controller/Volo.Controller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Volo.Controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
